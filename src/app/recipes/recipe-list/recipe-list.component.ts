f62ae7e0589d9e7f35f4068fa8de63bf777a6b9d
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2018-08-07 17:16:13 +0200

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
