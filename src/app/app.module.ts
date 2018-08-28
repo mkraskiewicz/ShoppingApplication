4cccea51bab048fb081692d5b0aa5fad5e7df42f
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2018-08-28 15:55:02 +0200

    Added custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipesComponent,
     RecipeDetailComponent,
     RecipeListComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, Renderer2, HostListener, Host, OnInit, HostBinding } from "../../../node_modules/@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit{
+
+    @HostBinding('class.open') isOpen: boolean = false;
+    constructor(private elemRef: ElementRef, private renderer: Renderer2){}
+
+    ngOnInit(){}
+
+    @HostListener('click') mouseHoverOn(eventData: Event){
+        console.log("ABC")
+        this.isOpen = !this.isOpen;
+        this.renderer.addClass(this.elemRef.nativeElement,'open');
+    }
+
+
+
+}
